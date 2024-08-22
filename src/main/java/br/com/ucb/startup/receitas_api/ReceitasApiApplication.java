e9efd6573200173b0033436a3cd22dd035b589b9
jlmeireles <joaomeireles@logusinfo.com.br>
2024-08-22 10:18:54 -0300

    Initial commit

---
+++ src/main/java/br/com/ucb/startup/receitas_api/ReceitasApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.ucb.startup.receitas_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReceitasApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReceitasApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/ucb/startup/receitas_api/ReceitasApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ucb.startup.receitas_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReceitasApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
